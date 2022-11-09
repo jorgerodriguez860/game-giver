a71e0800928f6aa6e6cca76a77de316386c05c3e
Jorge Rodriguez <rjorgeluis16@gmail.com>
2022-11-09 14:42:32 -0500

    switched data pull from API to mock db

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -7,27 +7,27 @@ import '../../css/PageLayout.css'
 const PageLayout = ({children}) => {
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    const getAllGames = () => {
-      fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_KEY}&search=uncharted&developers=naughty-dog`)
-      .then((res) => res.json())
-      .then((data) => dispatch(fillVault(data.results)))
-    }
-
-    getAllGames()
-  }, [])
-
   // useEffect(() => {
-  //   let gameData = [
-  //     {id: '123', background_image: 'pic1', name: 'game1', released: 2022, rating: 7},
-  //     {id: '456', background_image: 'pic2', name: 'game2', released: 2018, rating: 9},
-  //     {id: '789', background_image: 'pic3', name: 'game3', released: 2015, rating: 8},
-  //     {id: '012', background_image: 'pic4', name: 'game4', released: 2020, rating: 10},
-  //     {id: '345', background_image: 'pic5', name: 'game5', released: 2019, rating: 8}
-  //   ]
+  //   const getAllGames = () => {
+  //     fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_KEY}&search=uncharted&developers=naughty-dog`)
+  //     .then((res) => res.json())
+  //     .then((data) => dispatch(fillVault(data.results)))
+  //   }
 
-  //   dispatch(fillVault(gameData))
+  //   getAllGames()
   // }, [])
+
+  useEffect(() => {
+    let gameData = [
+      {id: '123', background_image: 'pic1', name: 'game1', released: 2022, rating: 7},
+      {id: '456', background_image: 'pic2', name: 'game2', released: 2018, rating: 9},
+      {id: '789', background_image: 'pic3', name: 'game3', released: 2015, rating: 8},
+      {id: '012', background_image: 'pic4', name: 'game4', released: 2020, rating: 10},
+      {id: '345', background_image: 'pic5', name: 'game5', released: 2019, rating: 8}
+    ]
+
+    dispatch(fillVault(gameData))
+  }, [])
   
   return (
     <>
